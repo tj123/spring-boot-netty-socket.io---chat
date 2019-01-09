4c490ca09df6d192e0f44308338e4b5952cb8d0e
tangjian <tang123jian@126.com>
2019-01-09 09:28:33 +0800

    修改配置

--- src/main/java/com/neo/config/InterceptorConfig.java
+++ src/main/java/com/neo/config/InterceptorConfig.java
@@ -6,11 +6,8 @@ package com.neo.config;
 
 import com.alibaba.fastjson.JSON;
 import com.neo.entity.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,9 +15,7 @@ import javax.servlet.http.HttpSession;
 import java.io.PrintWriter;
 
 
-public class InterceptorConfig implements HandlerInterceptor {
-
-    private static final Logger log = LoggerFactory.getLogger(InterceptorConfig.class);
+public class InterceptorConfig extends HandlerInterceptorAdapter {
 
     /**
      * 进入controller层之前拦截请求
@@ -44,17 +39,9 @@ public class InterceptorConfig implements HandlerInterceptor {
 //            response.sendRedirect(url);
             PrintWriter printWriter = response.getWriter();
             printWriter.write(JSON.toJSONString(new Result(-1, "please login")));
+            response.setContentType("application/json;charset=UTF-8");
             return false;
         }
     }
 
-    @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-//        log.info("--------------处理请求完成后视图渲染之前的处理操作---------------");
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-//        log.info("---------------视图渲染之后的操作-------------------------0");
-    }
 }
